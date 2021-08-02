b278de7a5bd917301c02372be3bd5ede874d5373
amyjprogrammer <amyjprogrammer@gmail.com>
2021-08-01 20:41:44 -0400

    Added CSV option and Heatmap

--- football_app.py
+++ football_app.py
@@ -28,4 +28,40 @@ def load_data(year):
     return playerstats
 playerstats = load_data(selected_year)
 
-playerstats
+#Sorting teams in the sidebar   
+sorted_by_team = sorted(playerstats.Tm.unique())
+selected_teams = st.sidebar.multiselect('Teams', sorted_by_team, sorted_by_team)
+
+#Sorting by position - sidebar
+unique_position = ['RB', 'QB', 'WR', 'FB', 'TE']
+selected_position = st.sidebar.multiselect('Position', unique_position, unique_position)
+
+#Filtering the data
+filtered_team_position = playerstats[(playerstats.Tm.isin(selected_teams)) & playerstats.Pos.isin(selected_position)]
+
+st.header('Display Player Stats of Selected Team(s)')
+st.write('Data Dimension: ' + str(filtered_team_position.shape[0]) + ' rows and ' + str(filtered_team_position.shape[1])+ ' columns.') 
+st.dataframe(filtered_team_position)
+
+#Download info as a CSV File
+def filedownload(file):
+    csv = file.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    return href
+
+st.markdown(filedownload(filtered_team_position), unsafe_allow_html=True)
+
+# Heatmap
+if st.button('Intercorrelation Heatmap'):
+    st.header('Intercorrelation Matrix Heatmap')
+    filtered_team_position.to_csv('output.csv',index=False)
+    df = pd.read_csv('output.csv')
+
+    corr = df.corr()
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+    with sns.axes_style("white"):
+        f, ax = plt.subplots(figsize=(7, 5))
+        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
+    st.pyplot(f)
