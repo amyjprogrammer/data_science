9cbe8c4c689395cc5af2b86a4cfe3f089ec13b0a
amyjprogrammer <amyjprogrammer@gmail.com>
2021-08-02 23:55:49 -0400

    Adding Stock prices for the Year

--- stock_sp500.py
+++ stock_sp500.py
@@ -1,11 +1,13 @@
 #Import Libraries
 
+from pandas.core.indexes import period
 import streamlit as st 
 import pandas as pd
 import base64
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import yfinance as yf
 
 st.title('S&P 500')
 
@@ -26,5 +28,50 @@ def load_data():
     return data 
 
 data = load_data()
+data
 
+#Different sectors for the S&P 500 companies
+different_sector = data['GICS Sector'].unique()
+different_sector
 
+group_sector = data.groupby('GICS Sector')
+g = group_sector.describe()
+g
+
+health_sector = group_sector.get_group('Health Care')
+health_sector
+
+#Download stock prices
+#checkout https://pypi.org/project/yfinance/ (for more info on usng yfinance)
+stock_data = yf.download(
+    tickers = list(data.Symbol),
+    period = 'ytd',
+    interval = '1d',
+    group_by = 'ticker',
+    auto_adjust = True, #default is False
+    prepost = True, #default is False
+    threads = True, #default is True
+    proxy = None #default is None
+)
+
+#stock data for Abbott Laboratories for this year
+info = stock_data['ABT']
+info
+
+#adding Date in a Column to use for Matplotlib
+abt = pd.DataFrame(stock_data['ABT'].Close)
+abt['Date'] = abt.index
+abt
+
+def price_plot(symbol):
+    data = pd.DataFrame(stock_data[symbol].Close)
+    data['Date'] = data.index
+    plt.fill_between(data.Date, data.Close, color='skyblue', alpha=0.3)
+    plt.plot(data.Date, data.Close, color='skyblue', alpha=0.8)
+    plt.xticks(rotation=90)
+    plt.xlabel('Date')
+    plt.ylabel("Closing Price")
+    return st.pyplot()
+
+graph = price_plot('ABT')
+graph
